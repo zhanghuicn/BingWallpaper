df22ff98ab11b7a1b184b559a16a8c68178a0a7f
zhang_hui <zhang_hui_cn@126.com>
2015-10-03 12:04:08 +0800

    if img exists, program does not download from website

--- BingWallpaper/Wallpaper.cs
+++ BingWallpaper/Wallpaper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Json;
 using System.Linq;
@@ -45,22 +46,30 @@ namespace BingWallpaper
             JsonValue jsonValue = JsonValue.Parse(strJson);
             JsonObject jsonObj = jsonValue as JsonObject;
             String strURL = jsonObj["images"][0]["url"].ToString();
-            //return strURL.Replace("_1920x1080.jpg", "_1366x768.jpg").Replace(@"\/", "/").Replace("\"", "");
 
             return strURL.Replace(@"\/", "/").Replace("\"", "");
         }
 
         public static void Set(String uri, Style style)
         {
-            System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
-
-            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            System.Drawing.Image img = null;
             if (null != _saveDir && !Directory.Exists(_saveDir))
                 Directory.CreateDirectory(_saveDir);
-
             string imgPath = Path.Combine(null == _saveDir ? Path.GetTempPath() : _saveDir
                                     , DateTime.Now.ToString("yyyy-MM-dd") + ".bmp");
-            img.Save(imgPath, System.Drawing.Imaging.ImageFormat.Bmp);
+            if (File.Exists(imgPath))
+            {
+                // 如果文件存在，就直接读取
+                img = Image.FromFile(imgPath);
+            }
+            else
+            {
+                // 如果文件不存在，就从URI中获取
+                img = Image.FromStream(new System.Net.WebClient().OpenRead(uri.ToString()));
+
+                // 将图片保存到本地目录
+                img.Save(imgPath, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
 
             RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
             if (style == Style.Stretched)
@@ -79,10 +88,11 @@ namespace BingWallpaper
                 key.SetValue(@"TileWallpaper", 1.ToString());
             }
 
-            SystemParametersInfo(SPI_SETDESKWALLPAPER,
-                0,
-                imgPath,
-                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            // 设置桌面壁纸的API
+            SystemParametersInfo(SPI_SETDESKWALLPAPER
+                                , 0
+                                , imgPath
+                                , SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
     }
 }
